2e48e6dffca6ad72a5a932bd74d50832c3fd1d6d
Samuel Lindberg <samuel.lindberg@jacks.sdstate.edu>
2022-03-27 21:08:50 -0500

    build working with android emulator

--- victrola_shop/lib/screens/landing_screen.dart
+++ victrola_shop/lib/screens/landing_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class LandingScreen extends StatelessWidget {
-  const LandingScreen({ Key? key }) : super(key: key);
+  const LandingScreen({Key? key}) : super(key: key);
 
   static String routeName = '/landing';
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextButton(),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Victrola Shop')),
     );
   }
 }
