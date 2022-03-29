f6b82fbc632ce5a9b63efba1ff7d5e0784cfe18c
Cimpecoven <cimpecoven@gmail.com>
2022-03-29 14:15:30 -0500

    Started user profile pages

---
+++ victrola_shop/lib/screens/ProfileScreens/profile_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileDetailScreen extends StatefulWidget {
+  const ProfileDetailScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<ProfileDetailScreen> createState() => _ProfileDetailScreenState();
+}
+
+class _ProfileDetailScreenState extends State<ProfileDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ victrola_shop/lib/screens/ProfileScreens/profile_list_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileListScreen extends StatefulWidget {
+  const ProfileListScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<ProfileListScreen> createState() => _ProfileListScreenState();
+}
+
+class _ProfileListScreenState extends State<ProfileListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
